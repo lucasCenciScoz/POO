ceab157c473eee20ff519723dc7435b5c68aa15f
lucasCenciScoz <lucascenciscoz@gmail.com>
2025-04-02 19:37:00 -0300

    Explicação do código

--- objetos/atividade1POO.js
+++ objetos/atividade1POO.js
@@ -1,12 +1,12 @@
 const prompt = require('prompt-sync')();
-
+// Modelo atributos e funções para a criação de um objeto
 class Cliente {
     nome;
     idade;
     cpf;
     agencia;
 }
-
+//Objeto que possui as informacoes da classe de funções para a criação de um objeto
 let cliente = new Cliente();
 
 cliente.nome = prompt("digite o nome do cliente:");
